f595253774eb642781504e942e798c7727b841df
Brandon Parris <brandonparrisguy@gmail.com>
2019-07-26 06:41:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,6 @@
  def katz_deli
    if there_is_nobody_in_line.length == 0 
      puts "The line is currently empty."
-   else there_are_people_in_line > 0 
+   else there_are_people_in_line.each > 0 
      puts there_are_people_in_line.index 
    end
