69a397df7c4c2a3a58ff6483bbcbb39b5aff4298
Brandon Parris <brandonparrisguy@gmail.com>
2019-07-26 05:42:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,4 @@
 # Write your code here.
- katz_deli = []
- return "The line is currently empty."
-
-def take_a_number(katz_deli, name)
-  length = self.count
-  index = 0 
-  while index < length 
-    yield (self[index])
-    index += 0 
-    end
-end
+ def line 
+   if line.length == 0 
+     puts "The line is currently empty."
