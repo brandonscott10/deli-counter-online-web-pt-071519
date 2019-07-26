4d87a6d0e465f1594effb0082f7c3f7a30e7a64a
Brandon Parris <brandonparrisguy@gmail.com>
2019-07-26 06:45:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,6 @@
    if there_is_nobody_in_line.length == 0 
      puts "The line is currently empty."
    else there_are_people_in_line.each do |line|
-     puts "The line is currently: #{line}"
+     puts "The line is currently: #{line}."
    end
  end
