420499b3c776dd39582de03ec4992218eff5aefe
Brandon Parris <brandonparrisguy@gmail.com>
2019-07-26 05:30:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,4 +3,11 @@
  return "The line is currently empty."
 
 def take_a_number(katz_deli, name)
-  length
+  length = self.count
+  index = 0 
+  while index < length 
+    yield (self[index])
+    index += 0 
+    end
+  end
+end
